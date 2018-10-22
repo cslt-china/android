6ed6c6d5a6e790b8bf71fc9f945f36372adbd970
Jianxia Xue <jxue@jxue.bej.corp.google.com>
2018-10-22 10:06:03 +0800

    Remove the dependencies of the deleted UI elements.
    Change-Id: I1e3918b793e5784b8f878d2aea875f78b49a96d7

--- app/src/main/java/com/google/android/apps/signalong/ReferenceVideoViewFragment.java
+++ app/src/main/java/com/google/android/apps/signalong/ReferenceVideoViewFragment.java
@@ -13,8 +13,6 @@ import com.google.android.apps.signalong.VideoViewFragment;
 
 public class ReferenceVideoViewFragment extends Fragment {
   private static final String TAG = "ReferenceVideoViewFrag";
-  private TextView titleTextView;
-  private TextView skipButton;
   private VideoViewFragment videoView;
 
   @Override
@@ -25,11 +23,6 @@ public class ReferenceVideoViewFragment extends Fragment {
 
     videoView = (VideoViewFragment) getChildFragmentManager().findFragmentById(
         R.id.fragment_reference_video_view);
-    titleTextView = (TextView)  viewContainer.findViewById(R.id.reference_video_title);
-    skipButton = (TextView) viewContainer.findViewById(R.id.skip_reference_button);
-
-    viewContainer.findViewById(R.id.skip_reference_button).setOnClickListener(
-        view -> videoView.stopPlayback());
 
     return viewContainer;
   }
@@ -38,17 +31,12 @@ public class ReferenceVideoViewFragment extends Fragment {
     videoView.setVideoViewCompletionListener(listener);
   }
 
-  public void setSkipClickListener(View.OnClickListener listener) {
-    skipButton.setOnClickListener(listener);
-  }
-
   public void stopPlayback() {
     videoView.stopPlayback();
   }
 
   public void playReference(String title, String videoPath) {
     Log.i(TAG, "play reference video for " + title);
-    titleTextView.setText(String.format(getString(R.string.demonstration), title));
     videoView.viewVideo(videoPath);
   }
 }
