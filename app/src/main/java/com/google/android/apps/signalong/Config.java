137b758611b2781e5dd5a329170ecf74f6a4a80b
xuguang <xuguang@google.com>
2018-10-24 17:25:32 +0800

    Modify the mothed of selecting video size
    select 1024x768 for record video
    modify some layout params of VideoView in fragment_video_view so
    VideoView can match the video aspect ratio.
    
    Change-Id: If720f8b49e873f616aecbb0ec1fae53ad9d1e690

---
+++ app/src/main/java/com/google/android/apps/signalong/Config.java
@@ -0,0 +1,9 @@
+package com.google.android.apps.signalong;
+
+public class Config {
+  //It is for landscape video.
+  //For portrait video, recorder will add a matrix to rotate the image
+  public static final int RECORD_VIDEO_WIDTH = 1024;
+  public static final int RECORD_VIDEO_HEIGHT = 768;
+
+}
