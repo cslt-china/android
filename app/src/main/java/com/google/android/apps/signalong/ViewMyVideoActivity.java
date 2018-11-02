8a3d2fcde0f8ba82888a354f891e37ebe762dba8
Jianxia Xue <jxue@google.com>
2018-11-02 18:57:32 +0800

    Use card view for the display of VideoListResponse.DataBeanList.DataBean.
    Change-Id: Iea6afc6205ac34f00e1e9d4eb279b19f2dfec22b

--- app/src/main/java/com/google/android/apps/signalong/ViewMyVideoActivity.java
+++ app/src/main/java/com/google/android/apps/signalong/ViewMyVideoActivity.java
@@ -10,12 +10,21 @@ import android.widget.TextView;
 import com.google.android.apps.signalong.jsonentities.VideoListResponse;
 import com.google.android.apps.signalong.utils.TimerUtils;
 import com.google.android.apps.signalong.utils.ToastUtils;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewMyVideoActivity extends BaseActivity {
   private static final String TAG = "ViewMyVideoActivity";
 
   // The intent param to pass video databean from caller activity.
   public static final String VIDEO_DATA_PARAM = "my_video_param";
+
+  private static final Map<String, Integer> GLOSS_STATUS_LABEL_ID_MAP = new HashMap<>();
+  static {
+    GLOSS_STATUS_LABEL_ID_MAP.put("APPROVED", R.string.label_gloss_status_approved);
+    GLOSS_STATUS_LABEL_ID_MAP.put("PENDING_APPROVAL", R.string.label_gloss_status_pending_approval);
+    GLOSS_STATUS_LABEL_ID_MAP.put("REJECTED", R.string.label_gloss_status_rejected);
+  }
 
   private VideoListResponse.DataBeanList.DataBean myVideoData;
   private VideoViewFragment videoView;
@@ -39,7 +48,7 @@ public class ViewMyVideoActivity extends BaseActivity {
     ((TextView) findViewById(R.id.gloss_creation_time_textview))
         .setText(TimerUtils.convertTimestamp(myVideoData.getCreatedTime()));
     ((TextView) findViewById(R.id.gloss_status_textview))
-        .setText(String.valueOf(myVideoData.getStatus()));
+        .setText(getString(GLOSS_STATUS_LABEL_ID_MAP.get(myVideoData.getStatus())));
     ((TextView) findViewById(R.id.gloss_approved_counter_textview))
         .setText(String.valueOf(myVideoData.getApprovedReviewCounter()));
     ((TextView) findViewById(R.id.gloss_rejected_counter_textview))
